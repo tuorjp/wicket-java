8a538f235ea701bff79670c8c57729b066592025
João Pedro <jplmarotinho22@gmail.com>
2025-10-29 12:44:39 -0300

    Download usando AjaxDownloadBehavior e AbstractResource, padrão do wicket 10

--- src/main/java/com/tuorjp/wicket_java/wicket/pages/home/HomePage.java
+++ src/main/java/com/tuorjp/wicket_java/wicket/pages/home/HomePage.java
@@ -2,11 +2,14 @@ package com.tuorjp.wicket_java.wicket.pages.home;
 
 import com.giffing.wicket.spring.boot.context.scan.WicketHomePage;
 import com.tuorjp.wicket_java.model.Todo;
+import com.tuorjp.wicket_java.service.ExcelGeneratorService;
 import com.tuorjp.wicket_java.service.MongoDBService;
 import com.tuorjp.wicket_java.wicket.pages.BasePage;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.extensions.ajax.AjaxDownloadBehavior;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.CheckBox;
@@ -18,17 +21,24 @@ import org.apache.wicket.markup.html.panel.FeedbackPanel;
 import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.resource.AbstractResource;
+import org.apache.wicket.request.resource.ContentDisposition;
+import org.apache.wicket.request.resource.IResource;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
+import java.io.IOException;
 import java.util.List;
 
 @WicketHomePage
 public class HomePage extends BasePage {
 
     @SpringBean
-    MongoDBService mongoDBService;
+    private MongoDBService mongoDBService;
 
-    FeedbackPanel feedbackPanel;
+    @SpringBean
+    private ExcelGeneratorService excelGeneratorService;
+
+    private FeedbackPanel feedbackPanel;
 
     LoadableDetachableModel<List<Todo>> todoListModel;
 
@@ -88,14 +98,45 @@ public class HomePage extends BasePage {
             }
         };
 
-        AjaxSubmitLink downloadExcelBtn = new AjaxSubmitLink("downloadExcelBtn", form) {
+        IResource dynamicExcelResource = new AbstractResource() {
             @Override
-            protected void onSubmit(AjaxRequestTarget target) {
+            protected ResourceResponse newResourceResponse(Attributes attributes) {
+                ResourceResponse response = new ResourceResponse();
 
+                response.setFileName("excel-todos.xlsx");
+                response.setContentDisposition(ContentDisposition.ATTACHMENT);
+
+                response.setWriteCallback(new WriteCallback() {
+                    @Override
+                    public void writeData(Attributes attributes) throws IOException {
+                        try {
+                            List<Todo> todos = todoListModel.getObject();
+                            Workbook wb = excelGeneratorService.createExcelFile(todos);
+
+                            wb.write(attributes.getResponse().getOutputStream());
+                            wb.close();
+                        } catch (Exception e) {
+                            throw new IOException("Erro ao gerar o arquivo Excel", e);
+                        }
+                    }
+                });
+
+                return response;
+            }
+        };
+
+        AjaxDownloadBehavior downloadExcel = new AjaxDownloadBehavior(dynamicExcelResource);
+
+
+        AjaxLink<Void> downloadExcelBtn = new AjaxLink<Void>("downloadExcelBtn") {
+            @Override
+            public void onClick(AjaxRequestTarget ajaxRequestTarget) {
+                downloadExcel.initiate(ajaxRequestTarget);
             }
         };
 
         form.add(btnAdd, btnRemove, downloadExcelBtn);
+        form.add(downloadExcel);
 
         //adicionando formulário invisível de novo item dentro do form principal
         formNew.setOutputMarkupPlaceholderTag(true);
